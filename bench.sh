f94d048b14396e571fbb6066be7c2c542208a4d9
William Poussier <william.poussier@gmail.com>
2019-09-19 17:21:57 +0200

    chore: add GitHub actions CI workflow

---
+++ bench.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Starting benchmark..."
+
+# Execute benchmarks multiple times.
+for i in {1..1}
+do
+   echo " + run #$i"
+   go test -bench=. >> .benchruns
+done
+
+echo -e "\nStatistics"
+echo "=========="
+
+# Use an absolute path because the setup-go
+# GitHub action does not set the PATH env var.
+# See https://github.com/actions/setup-go/issues/14.
+"$(go env GOPATH)/bin/benchstat" .benchruns | tee benchstats
+rm -f .benchruns
